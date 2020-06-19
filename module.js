269b0f052836a159d46e61f8a548a9009ef2f407
Jonrrile <jonrrile@ut.utm.edu>
2020-06-19 13:54:42 -0500

    filtered succesfully for businesses in NY and then for businesses only in the Manufacturing industry.

--- module.js
+++ module.js
@@ -1,19 +1,33 @@
 import businesses from "./data.js"
 
-
 const outEl = document.querySelector("#output")
-outEl.innerHTML = "<h1>Active Businesses</h1>"
+outEl.innerHTML = "<h1>Manufacturing Businesses</h1>"
 
-businesses.forEach(business => {
+const manufacturingBusinesses = businesses.filter(business => {
+    let manufacturing = false
+  
+    if (business.companyIndustry === "Manufacturing") {
+        manufacturing = true
+    }
+  
+    return manufacturing
+  })
+
+  console.log(manufacturingBusinesses)
+
+
+manufacturingBusinesses.forEach(manufacturingBusinesses => {
   outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
+    <h2>${manufacturingBusinesses.companyName}</h2>
     <section>
-      ${business.addressFullStreet}
+      ${manufacturingBusinesses.addressFullStreet}
     </section>
     <section>
-    ${business.addressCity}, ${business['addressStateCode']}, ${business['addressZipCode']}
+    ${manufacturingBusinesses.addressCity}, ${manufacturingBusinesses['addressStateCode']}, ${manufacturingBusinesses['addressZipCode']}
   `
   outEl.innerHTML += "<hr/>"
 });
+
+// Array to contain all the New York businesses
 
 export default outEl; 
