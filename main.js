e7a13afee6d24eb7cfb61124e16c0203ae919bfc
Jonrrile <jonrrile@ut.utm.edu>
2020-06-19 14:44:12 -0500

    trying to get numbers to match up with agents

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import businesses from "./data.js"
 import outE1 from "./module.js"
-
+import outE2 from "./module.js"
 
--- module.js
+++ module.js
@@ -1,33 +1,52 @@
 import businesses from "./data.js"
 
 const outEl = document.querySelector("#output")
-outEl.innerHTML = "<h1>Manufacturing Businesses</h1>"
+outEl.innerHTML += "<h1>Purchasing Agents</h1>";
 
-const manufacturingBusinesses = businesses.filter(business => {
-    let manufacturing = false
-  
-    if (business.companyIndustry === "Manufacturing") {
-        manufacturing = true
-    }
-  
-    return manufacturing
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+    return business.purchasingAgent
+
+})
+
+const agentNumber = businesses.map(business => {
+    return business.phoneWork
+})
+
+console.table(agents)
+//console.table(agentNumber)
+
+agents.forEach(agent => {
+  outEl.innerHTML += `<h2 id="details">${agent.nameFirst} ${agent.nameLast}</h2>`;
+  outEl.innerHTML += "<hr/>";
+})
+
+/*
+agentNumber.forEach(business => {
+    outEl.innerHTML += `<section>${business.phoneWork}</section>`
+})
+*/
+//const outEl = document.querySelector("#details")
+  businesses.forEach(business => {
+      outEl.innerHTML += `
+      <sectoin>${business.phoneWork}</section>
+      <section>
+      ${business.addressCity}</section>`
   })
 
-  console.log(manufacturingBusinesses)
 
-
-manufacturingBusinesses.forEach(manufacturingBusinesses => {
-  outEl.innerHTML += `
-    <h2>${manufacturingBusinesses.companyName}</h2>
-    <section>
-      ${manufacturingBusinesses.addressFullStreet}
-    </section>
-    <section>
-    ${manufacturingBusinesses.addressCity}, ${manufacturingBusinesses['addressStateCode']}, ${manufacturingBusinesses['addressZipCode']}
-  `
-  outEl.innerHTML += "<hr/>"
-});
-
-// Array to contain all the New York businesses
-
+/*const outE2 = document.querySelector("#details")
+businesses.forEach(business => {
+    outEl.innerHTML += `
+      <section>${business.phoneWork}</section>
+      <section>
+        ${business.addressFullStreet}
+      </section>
+    `
+    outE2.innerHTML += "<hr/>"
+  });
+*/
 export default outEl; 
